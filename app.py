9bd80c1f57a1a2612575ec40d23a9204cee25b24
anema25 <116646218+anema25@users.noreply.github.com>
2023-12-18 20:18:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from joblib import load
+
+# Load your models
+presence_classifier = load('api/presence_classifier.joblib')
+presence_vect = load('api/presence_vectorizer.joblib')
+category_classifier = load('api/category_classifier.joblib')
+category_vect = load('api/category_vectorizer.joblib')
+
+# Streamlit app
+def main():
+    st.title("Dark Pattern Detection")
+
+    # Get user input
+    tokens = st.text_area("Enter tokens (one per line):", "").split('\n')
+
+    if st.button("Detect Dark Patterns"):
+        output = detect_dark_patterns(tokens)
+        st.json({"result": output})
+
+def detect_dark_patterns(tokens):
+    output = []
+
+    for token in tokens:
+        result = presence_classifier.predict(presence_vect.transform([token]))
+        if result == 'Dark':
+            cat = category_classifier.predict(category_vect.transform([token]))
+            output.append(cat[0])
+        else:
+            output.append(result[0])
+
+    dark_patterns = [tokens[i] for i in range(len(output)) if output[i] == 'Dark']
+    st.text("Detected Dark Patterns:")
+    for d in dark_patterns:
+        st.text(d)
+    
+    st.text(f"Number of Detected Dark Patterns: {len(dark_patterns)}")
+
+    st.text("Debugging:")
+    st.text(f"Output of presence classifier: {output}")
+    
+    return output
+
+if __name__ == '__main__':
+    main()
